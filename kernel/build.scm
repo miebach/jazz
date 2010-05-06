miebach/jazz

commit 88dfa8212220afdf0802aa25ff65e3b3f9d1c704

Build script now calls the kernel without changing the current-directory

kernel/build.scm
@@ -992,8 +992,7 @@
       (jazz.call-process (kernel-path) `("-:dq-" "-make"
                                          ,(symbol->string product)
                                          ,@(if link `("-link" ,(symbol->string link)) '())
-                                         ,@(if jobs `("-jobs" ,(number->string jobs)) '()))
-        destdir)))
+                                         ,@(if jobs `("-jobs" ,(number->string jobs)) '())))))
   
   (jazz.make-kernel configuration #f #f)
   (product-make product configuration jobs))

kernel/runtime/base.scm
@@ -448,12 +448,11 @@
 ;;;
 
 
-(define (jazz.call-process path arguments #!optional (directory #f))
+(define (jazz.call-process path arguments)
   (let ((port (open-process
                 (list
                   path: path
                   arguments: arguments
-                  directory: (or directory (current-directory))
                   stdin-redirection: #f
                   stdout-redirection: #f
                   stderr-redirection: #f))))

commit 43e0445dcd3e0dc5feb56bdb2fe37fed3dd2b1b3

Fixed path to kernel in build scripts for non-Windows platforms.
(Broke in 88dfa8212220afdf0802 "Build script now calls the kernel...")

kernel/build.scm
@@ -976,26 +976,13 @@
 
 
 (define (jazz.make-product product configuration link jobs)
-  (define (product-make product configuration jobs)
-    (let ((destdir (jazz.configuration-directory configuration))
-          (platform (jazz.configuration-platform configuration)))
-      (define (build-file path)
-        (string-append destdir path))
-      
-      (define (kernel-path)
-        (case platform
-          ((windows)
-           (build-file "kernel"))
-          (else
-           "./kernel")))
-      
-      (jazz.call-process (kernel-path) `("-:dq-" "-make"
-                                         ,(symbol->string product)
-                                         ,@(if link `("-link" ,(symbol->string link)) '())
-                                         ,@(if jobs `("-jobs" ,(number->string jobs)) '())))))
-  
   (jazz.make-kernel configuration #f #f)
-  (product-make product configuration jobs))
+  (jazz.call-process
+     (string-append (jazz.configuration-directory configuration) "kernel")
+     `("-:dq-" "-make"
+       ,(symbol->string product)
+       ,@(if link `("-link" ,(symbol->string link)) '())
+       ,@(if jobs `("-jobs" ,(number->string jobs)) '())))(product-make product configuration jobs))
 
 
 ;;;

kernel/runtime/unit.scm
@@ -1373,13 +1373,6 @@
         (stop-build? #f))
     
     (define (grab-build-process)
-      (define (kernel-path)
-        (case jazz.kernel-platform
-          ((windows)
-           (%%string-append jazz.kernel-install "kernel"))
-          (else
-           "./kernel")))
-      
       (mutex-lock! process-mutex)
       (let ((active-count (%%length active-processes))
             (jobs (or jazz.jobs (jazz.build-jobs))))
@@ -1391,7 +1384,7 @@
               ((%%fx< active-count jobs)
                (let ((process (open-process
                                 (list
-                                  path: (kernel-path)
+                                  path: (%%string-append jazz.kernel-install "kernel")
                                   arguments: `("-:dq-" "-build" ,(%%number->string active-count) ,@(if jazz.link `("-link" ,(%%symbol->string jazz.link)) '()) "-jobs" "1")
                                   stdin-redirection: #t
                                   stdout-redirection: #t